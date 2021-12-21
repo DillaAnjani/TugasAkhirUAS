87a4fabbd83b4eaa0a9f9176b7f48e9a4b852f1f
DillaAnjani <90698905+DillaAnjani@users.noreply.github.com>
2021-12-21 12:43:26 +0700

    Add files via upload

---
+++ bab3/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    $('p').css('border','2px white');
+
+    $('.tabular-header').css('background-color','#9AE66E');
+
+    $('input:submit').css('background-color','green');
+    $('input:button').css('background-color','red');
+
+    $('.btn').css('display':'inline-block',
+        'background':'#FF5151',
+        'padding':'14px 0',
+        'color':'white',
+        'text-transform':'uppercase',
+        'cursor':'pointer',
+        'margin-top':'8px',
+        'width':'100%');
+})
