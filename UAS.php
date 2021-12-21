c68c0b4dba37ef375035c7c6dc0050c4cae7a895
DillaAnjani <90698905+DillaAnjani@users.noreply.github.com>
2021-12-21 20:16:26 +0700

    SOAL11

--- UAS.php
+++ UAS.php
@@ -3,17 +3,95 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TABEL PERULANGAN</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+        }
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+    </style>
 </head>
 <body>
-<?php
-$a = 50;
-$b = 10;
-if($a == $b) {
-     echo 'Benar';
-} else
-{
-     echo 'Salah';
-}
-?>
+    <table>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>2</td>
+            <th>3</th>
+            <td>4</td>
+            <th>5</th>
+        </tr>
+    </table>
 </body>
 </html>
