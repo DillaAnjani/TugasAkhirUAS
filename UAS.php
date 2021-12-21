c8212b56496e30b13d69a52b765f7b3a5195fd0f
DillaAnjani <90698905+DillaAnjani@users.noreply.github.com>
2021-12-21 21:02:20 +0700

    SOAL15

--- UAS.php
+++ UAS.php
@@ -1,21 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>PERNYATAAN BENAR DAN SALAH</title>
-</head>
-<body>
-<?php
-$color = "red";
-switch ($color){
-    case "red":
-        echo "BENAR";
-    break;
-    case "green":
-        echo  "SALAH";
-    break;
-}
-?>
-</body>
+<html>
+    <p id="halo"></p>
+
+    <script>
+        document.getElementById("halo").innerHTML = "Demo";
+    </script>
+
 </html>
