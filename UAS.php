7a13f5c0d0ada58a74cdfc193d3ae2ee0233d9d6
DillaAnjani <90698905+DillaAnjani@users.noreply.github.com>
2021-12-21 20:41:44 +0700

    SOAL12

--- UAS.php
+++ UAS.php
@@ -3,95 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TABEL PERULANGAN</title>
-    <style>
-        table {
-            border-collapse: collapse;
-        }
-        table, th, td {
-            border: 1px solid black;
-        }
-        th, td {
-            padding: 10px;
-        }
-        th {
-            background-color: #4CAF50;
-            color: white;
-        }
-    </style>
 </head>
 <body>
-    <table>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-        <tr>
-            <th>1</th>
-            <td>2</td>
-            <th>3</th>
-            <td>4</td>
-            <th>5</th>
-        </tr>
-    </table>
+<?php
+$i = 1;
+
+for($i = 1; $i < 6; $i++){
+    echo $i;
+}
+?>
 </body>
 </html>
